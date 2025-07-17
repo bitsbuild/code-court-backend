58f284b93d4e7c04a17637a795415ab3ed69269a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-17 08:09:56 +0530

    Apps And Rest Framework Registered At Project Level Settings

--- codecourt/codecourt/settings.py
+++ codecourt/codecourt/settings.py
@@ -13,6 +13,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user',
+    'app',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
